d78d3db325c4f8eeacd01573e0c09c72bca8101a
SSSSSea6 <sssssea666@gmail.com>
2025-08-29 12:09:27 +0800

    Create search.js

---
+++ static/js/search.js
@@ -0,0 +1,33 @@
+const searchClient = algoliasearch(
+  'SMA85IWFQN',        // <--- ！！！在这里填入你自己的 Application ID
+  '8a00c461ba4a78c16cb8862108e7e83a'  // <--- ！！！在这里填入你自己的 Search-Only API Key
+);
+
+const search = instantsearch({
+  indexName: 'nuaaguide',
+  searchClient,
+});
+
+search.addWidgets([
+  instantsearch.widgets.searchBox({
+    container: '#search-box',
+    placeholder: '输入课程名、老师或关键词搜索...',
+  }),
+
+  instantsearch.widgets.hits({
+    container: '#hits',
+    templates: {
+      item: `
+        <article>
+          <a href=" ">
+            <h4>{{#helpers.highlight}}{ "attribute": "title" }{{/helpers.highlight}}</h4>
+          </a >
+          <p>{{#helpers.highlight}}{ "attribute": "summary" }{{/helpers.highlight}}</p >
+        </article>
+      `,
+      empty: `没有找到与 "{{ query }}" 相关的资料。`,
+    },
+  })
+]);
+
+search.start();
