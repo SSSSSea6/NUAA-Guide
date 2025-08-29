617dff02410d69aea51da0d20867916eae88aca5
SSSSSea6 <sssssea666@gmail.com>
2025-08-29 12:24:17 +0800

    Update search.js

--- static/js/search.js
+++ static/js/search.js
@@ -14,20 +14,20 @@ search.addWidgets([
     placeholder: '输入课程名、老师或关键词搜索...',
   }),
 
-  instantsearch.widgets.hits({
-    container: '#hits',
-    templates: {
-      item: `
-        <article>
-          <a href=" ">
-            <h4>{{#helpers.highlight}}{ "attribute": "title" }{{/helpers.highlight}}</h4>
-          </a >
-          <p>{{#helpers.highlight}}{ "attribute": "summary" }{{/helpers.highlight}}</p >
-        </article>
-      `,
-      empty: `没有找到与 "{{ query }}" 相关的资料。`,
-    },
-  })
+ instantsearch.widgets.hits({
+  container: '#hits',
+  templates: {
+    item: `
+      <article>
+        <a href=" " style="color: #0056b3; font-weight: bold;">
+          <h4>{{#helpers.highlight}}{ "attribute": "title" }{{/helpers.highlight}}</h4>
+        </a >
+        <p style="color: #6c757d; margin-top: 4px;">{{#helpers.highlight}}{ "attribute": "summary" }{{/helpers.highlight}}</p >
+      </article>
+    `,
+    empty: `没有找到与 "{{ query }}" 相关的资料。`,
+  },
+})
 ]);
 
 search.start();
