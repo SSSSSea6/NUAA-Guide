814eec3d04802ca07df39b87070d14bafb6eebad
SSSSSea6 <sssssea666@gmail.com>
2025-08-29 23:37:11 +0800

    Update search.js

--- static/js/search.js
+++ static/js/search.js
@@ -1,6 +1,6 @@
 const searchClient = algoliasearch(
-  'SMA85IWFQN',        // <--- ！！！在这里填入你自己的 Application ID
-  '8a00c461ba4a78c16cb8862108e7e83a'  // <--- ！！！在这里填入你自己的 Search-Only API Key
+  'SMA85IWFQN',        // 确保这里是你自己的 Application ID
+  '8a00c461ba4a78c16cb8862108e7e83a'  // 确保这里是你自己的 Search-Only API Key
 );
 
 const search = instantsearch({
@@ -8,26 +8,7 @@ const search = instantsearch({
   searchClient,
 });
 
-search.addWidgets([
-  instantsearch.widgets.searchBox({
-    container: '#search-box',
-    placeholder: '输入课程名、老师或关键词搜索...',
-  }),
-
- instantsearch.widgets.hits({
-  container: '#hits',
-  templates: {
-    item: `
-      <article>
-        <a href="{{permalink}}" style="color: #0056b3; font-weight: bold;">
-          <h4>{{#helpers.highlight}}{ "attribute": "title" }{{/helpers.highlight}}</h4>
-        </a >
-        <p style="color: #6c757d; margin-top: 4px;">{{#helpers.highlight}}{ "attribute": "summary" }{{/helpers.highlight}}</p >
-      </article>
-    `,
-    empty: `没有找到与 "{{ query }}" 相关的资料。`,
-  },
-})
-]);
+search.addWidgets();
 
 search.start();
+
